003cededcd9ade3c14089ec51463f6c80a556953
mgardos01 <mgardos123@gmail.com>
2024-01-01 15:45:10 -0800

    fixed main site

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,21 @@
-<svelte:head>  
+<svelte:head>
     <title>Max Gardos Portfolio</title>
 </svelte:head>
-  
-<div id="container">
-<div id="title">
-    <img src="../lib/githubpfp.png" alt="Github PFP." height="250px">
-</div>
-<div id="links">
-    <a href="https://github.com/mgardos01">Github</a>
-    <a href="https://www.linkedin.com/in/max-gardos-8a1bb21a1/">LinkedIn</a>
-</div>
-</div>
+
+<body>
+    <main>
+        <div id="container">
+            <div id="title">
+                <img
+                    src="../lib/githubpfp.png"
+                    alt="Github PFP."
+                    height="250px"
+                />
+            </div>
+            <div id="links">
+                <a href="https://github.com/mgardos01">Github</a>
+                <a href="https://www.linkedin.com/in/max-gardos-8a1bb21a1/">LinkedIn</a>
+            </div>
+        </div>
+    </main>
+</body>
